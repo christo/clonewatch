496d434fac86c1cb59e830aa229e2e3842f3a103
Chris Mountford <chris@think.net.au>
2024-05-21 10:41:03 +1000

    script for starting in tmux with log tailing pane

---
+++ tmux-clonewatch.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+# using tmux session named cw
+# start clonewatch with its logs tailing in two panes of a new tmux window
+# if such a session exists, attach to it, otherwise create it
+
+set -eu
+
+if (tmux has-session -t cw 2>/dev/null); then
+    tmux attach -t cw
+else
+    # create new session named cw
+    # -u means utf8 -2 means 256 colour
+    # -t foo:bar sets target session "foo" new window "bar"
+    # -d start detached (will attach at end)
+    tmux -u2 new-session -s cw -n clonewatch -d
+    tmux select-window -t cw:clonewatch
+    tmux send-keys -t cw:clonewatch "clonewatch.py" Enter
+    # vertical split (top and bottom panes), new pane receives focus
+    tmux split-window -v
+    tmux send-keys -t cw "tail -f $(clonewatch.py -l)" Enter
+    tmux select-layout even-vertical
+    # now attach to it
+    tmux -u2 attach -t cw
+fi
